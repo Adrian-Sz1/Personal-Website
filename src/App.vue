a37f0e484f99f1d9d2308a05a7f7c3a5f0418f9e
Adrian-Sz1 <aszoszkiewicz01@qub.ac.uk>
2025-03-26 15:33:21 +0000

    Hotfix Change profile pic path

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/NavBar.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/profile-pic.jpg" width="125" height="125"/>
+    <img alt="Vue logo" class="logo" src="../src/assets/profile-pic.jpg" width="125" height="125"/>
 
     <div class="wrapper">
       <HelloWorld msg="Adrian Szoszkiewicz" />
