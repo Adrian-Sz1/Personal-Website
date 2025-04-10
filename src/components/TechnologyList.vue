3b9a0696381b38cf1de22a31c03b15e519353a17
Adrian-Sz1 <105881872+Adrian-Sz1@users.noreply.github.com>
2025-04-10 13:21:48 +0100

    Refactor ContentBox + unit tets
    - Refactor ContentBox.vue by splitting technologylist in to its own testable component
    - Add tests for TechnologyList.vue component
    - Add data-testids for Navbar for easier E2E testing
    - Refactor config to take into account the new test folder and to ignore e2e tests
    - Remove the old __tests__ folder in favour of the new tests/ folder in root of the project

---
+++ src/components/TechnologyList.vue
@@ -0,0 +1,49 @@
+<script setup>
+const props = defineProps({
+  technologies: {
+    type: Array,
+    required: true,
+    validator: (value) => {
+      if (!Array.isArray(value)) {
+        return false;
+      }
+      return value.every((item) => typeof item === 'string');
+    },
+  },
+});
+</script>
+
+<template>
+  <ul id="skills-container">
+    <li class="mint-pill-medium" v-for="technology in technologies" :key="technology">
+      {{ technology }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+#out-skills-container {
+    flex-grow: 1;
+}
+
+ul {
+    padding: 0;
+}
+
+li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px;
+    padding: 0 10px 0 10px;
+    margin: 0.35rem;
+    margin-left: 0;
+}
+
+#skills-container {
+    list-style-type: none;
+    display: flex;
+    justify-content: left;
+    flex-wrap: wrap;
+}
+</style>
